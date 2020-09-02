62fca3daf3dedd241ed040a56469a417078a391f
nmsohn <odna425@gmail.com>
2020-09-02 20:45:59 +1200

    Added winston

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,1 +1,28 @@
-import { Logger, createLogger, format, transports } from "winston";
+import { Logger, createLogger, format, transports, transport } from "winston";
+
+const options = {
+	file: {
+		level: "info",
+		filename: "",
+		handleExceptions: true,
+		json: true,
+		maxFiles: 2,
+		maxSize: 1024 * 1024 * 5
+	},
+	console: {
+		level: "debug",
+		colorize: true,
+		handleExceptions: true,
+		json: false
+	}
+};
+
+const logger: Logger = createLogger({
+	format: format.combine(
+		format.timestamp({
+			format: "YYYY-MM-DD HH:mm:ss"
+		}),
+		format.prettyPrint()
+	),
+	transports: [new transports.File(options.file), new transports.Console(options.console)]
+});
