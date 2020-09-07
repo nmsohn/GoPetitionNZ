244a2310291107700dcb3160a74fc509c13ac123
nmsohn <odna425@gmail.com>
2020-09-07 15:13:59 +1200

    Updated logger class

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,30 +1,54 @@
-import { Logger, createLogger, format, transports, transport } from "winston";
+import * as winston from "winston";
+import * as dotenv from "dotenv";
 
-const options = {
-	file: {
-		level: "info",
-		filename: "",
-		handleExceptions: true,
-		json: true,
-		maxFiles: 2,
-		maxSize: 1024 * 1024 * 5
-	},
-	console: {
-		level: "debug",
-		colorize: true,
-		handleExceptions: true,
-		json: false
+export class Logger {
+	private logger: winston.Logger | undefined;
+	private env: string;
+
+	constructor(env: string) {
+		this.env = env;
+		this.logger = undefined;
 	}
-};
 
-const logger: Logger = createLogger({
-	format: format.combine(
-		format.timestamp({
-			format: "YYYY-MM-DD HH:mm:ss"
-		}),
-		format.prettyPrint()
-	),
-	transports: [new transports.File(options.file), new transports.Console(options.console)]
-});
+	public async init(): Promise<winston.Logger> {
+		const options = {
+			file: {
+				level: "info",
+				filename: "",
+				handleExceptions: true,
+				json: true,
+				maxFiles: 2,
+				maxSize: 1024 * 1024 * 5
+			},
+			console: {
+				level: "debug",
+				colorize: true,
+				handleExceptions: true,
+				json: false
+			}
+		};
 
-export default logger;
+		this.logger = winston.createLogger({
+			exitOnError: false,
+			format: winston.format.combine(
+				winston.format.timestamp({
+					format: "YYYY-MM-DD HH:mm:ss"
+				}),
+				winston.format.errors({ stack: true }),
+				winston.format.splat(),
+				winston.format.json()
+				// winston.format.prettyPrint()
+			),
+			transports: [new winston.transports.File(options.file)]
+		});
+
+		if (this.env !== "production") {
+			this.logger.clear();
+			this.logger.add(new winston.transports.Console(options.console));
+		}
+
+		return this.logger;
+	}
+}
+
+export default Logger;
