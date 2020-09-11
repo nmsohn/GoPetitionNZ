8a5fba431df4752ebcb71f2e33d753870f5722c5
nmsohn <odna425@gmail.com>
2020-09-11 19:47:11 +1200

    Removed connector

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express, { Application, Request, Response } from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
-import Connector from "./utils/connector";
 import { LogStream } from "./utils/logger";
 import morgan from "morgan";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 class App {
 	public app: Application;
@@ -20,5 +20,6 @@ class App {
 		this.app.use(bodyParser.json());
 		this.app.use(bodyParser.urlencoded({ extended: false }));
 		this.app.use(helmet());
+		this.app.use(cookieParser());
 	}
 }
