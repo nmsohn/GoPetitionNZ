16f0c87c24e0aa1405c1e7b5715a4e19479a635d
nmsohn <odna425@gmail.com>
2020-09-14 22:29:07 +1200

    Removed unused config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -10,11 +10,6 @@ class Config implements IConfig {
 		dotenv.config();
 	}
 
-	// public getMongoUrl(): string {
-	// 	const url = process.env.NODE_ENV == "production" ? process.env.PROD_DB_URL : process.env.DEV_DB_URL;
-	// 	return url ? url : "";
-	// }
-
 	public getEnvironmentName(): string {
 		const env = process.env.NODE_ENV ?? "development";
 		return env;
